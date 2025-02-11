a86b3dba55a7f6ee59d0450f804c622bad104472
kristapelayo <pelayokrista29@gmail.com>
2025-02-11 17:50:42 +0800

    personalities

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: 'https://kristapelayo.github.io/personalities',
-});
+  plugins: [react()],
+  base: "/personalities/"
+})
